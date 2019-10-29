51c93ef76a594e400925ae89cfd6e6923ca88a7a
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2019-10-29 23:20:00 +0200

    Tracking (initial version) finished but untested

--- common/tracking.js
+++ common/tracking.js
@@ -1,32 +1,28 @@
 /*
  * Using array2 to track multiple different things and share info between bots by using sharedB.
- * 1-9: enemy locations
- * 11-19: shield priorities
- * 0: pointer for next available enemy location number (1 to 9, with overflow back to 1)
- * 10: pointer for next available shield priorities (11 to 19, with overflow back to 11)
+ * See initializeTracking to understand what is tracked and where.
  *
  * Value encoding:
- *      000400031210
- *      ppppttttxxyy (priority-turn-x-y)
- *      priority is just for shield priorities. in the case of enemy location, priority is 0.
- *      priority is measured as distance to closest enemy bot, lower number means higher priority
- *      when a bot is shielded, its priority should be incremented
- *
+ *      0093000400031210
+ *      AAAABBBBttttxxyy (valA-valB-turn-x-y)
+ *      x,y are coordinates
+ *      turn is the turn when this tracking information was saved
+ *      valA, valB are additional information which can be saved if needed
  */
 
 
 initializeTracking = function() {
     POINTER_NEXT_AVAILABLE_ENEMY_INDEX = 0
     MIN_ENEMY_LOCATION_INDEX = 1
-    MAX_ENEMY_LOCATION_INDEX = 9
-    POINTER_NEXT_AVAILABLE_SHIELD_INDEX = 10
-    MIN_SHIELD_PRIO_INDEX = 11
-    MAX_SHIELD_PRIO_INDEX = 19
+    MAX_ENEMY_LOCATION_INDEX = 19
+    POINTER_NEXT_AVAILABLE_SHIELD_INDEX = 20
+    MIN_SHIELD_PRIO_INDEX = 21
+    MAX_SHIELD_PRIO_INDEX = 29
 
     if (!exists(sharedB)) {
         array2 = []
-        array2[0] = MIN_ENEMY_LOCATION_INDEX
-        array2[10] = MIN_SHIELD_PRIO_INDEX
+        array2[POINTER_NEXT_AVAILABLE_ENEMY_INDEX] = MIN_ENEMY_LOCATION_INDEX
+        array2[POINTER_NEXT_AVAILABLE_SHIELD_INDEX] = MIN_SHIELD_PRIO_INDEX
         sharedB = array2
     }
 
@@ -49,7 +45,7 @@ saveEncodedValue = function(enc, iPointer, iFirstVal, iLastVal) {
 }
 
 enemySpottedAt = function(cx, cy) {
-    enc = encodeTarget(0, turn, cx, cy)
+    enc = encode(0,0, turn, cx, cy)
     saveEncodedValue(enc, POINTER_NEXT_AVAILABLE_ENEMY_INDEX, MIN_ENEMY_LOCATION_INDEX, MAX_ENEMY_LOCATION_INDEX)
 }
 
@@ -62,8 +58,13 @@ updateEnemyLocations = function() {
     }
 }
 
+/*
+*      priority is measured as distance to closest enemy bot, lower number means higher priority
+*      when a bot is shielded, its priority should be incremented
+*/
+
 askForShield = function(priority, t, cx, cy) {
-    enc = encodeTarget(priority, t, cx, cy)
+    enc = encode(0, priority, t, cx, cy)
     saveEncodedValue(enc, POINTER_NEXT_AVAILABLE_SHIELD_INDEX, MIN_SHIELD_PRIO_INDEX, MAX_SHIELD_PRIO_INDEX)
 }
 
@@ -77,8 +78,8 @@ pollPrioritizedShieldWithinRange = function() {
             // Shield target may be out of our range.
             distToShieldTarget = abs(x - decodeX(enc)) + abs(y - decodeY(enc))
             if (distToShieldTarget <= 4) {
-                // Make sure shield was requested this turn. (assuming that requesting units act before shielding units. TODO: is this ok?)
-                if (decodeTurn(enc) == turn) {
+                // Make sure shield was requested this turn or last turn (because the shielding unit may be acting _before_ the requesting unit).
+                if (decodeTurn(enc) >= turn-1) {
                     // Set this to bestShield if it's the first good shield target we've seen or if it has smaller priority than previous bestShield.
                     if (bestShield < 0 || decodePriority(enc) < decodePriority(bestShield)) {
                         bestShield = enc
@@ -97,18 +98,31 @@ pollPrioritizedShieldWithinRange = function() {
 
 /**************************** Encoding/decoding values. **********************************/
 
-// TODO päivitä encoding/decoding toimimaan priojen kanssa
-
-encodeTarget = function(priority, t, cx, cy) {
-    return t*10000 + cx*100 + cy
+// Note: a <= 9006 due to JS integer limit.
+encode = function(a, b, t, cx, cy) {
+    if (a > 9006 || b > 9999 || t > 9999 || cx > 99 || cy > 99) debugLog("encodeError a="+a+",b="+b+",t="+t+",cx="+cx+",cy="+cy)
+    return a*1000000000000 + b*100000000 + t*10000 + cx*100 + cy
 }
 
-decodePriority = function(enc) {
+/*
+ *      0093000400031210
+ *      AAAABBBBttttxxyy (valA-valB-turn-x-y)
+ */
 
+decodePriority = function(enc) {
+    return decodeB(enc)
+}
+
+decodeA = function(enc) {
+    return floor(enc / 1000000000000)
+}
+
+decodeB = function(enc) {
+    return floor((enc % 1000000000000) / 100000000)
 }
 
 decodeTurn = function(enc) {
-    return floor(enc / 10000)
+    return floor((enc % 100000000) / 10000)
 }
 
 decodeX = function(enc) {
