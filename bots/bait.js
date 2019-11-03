5e309cb17d5c561dea804478a6652d7b5024be01
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2019-11-03 23:19:08 +0200

    Bait attempts to shield rushers in the beginning.

--- bots/bait.js
+++ bots/bait.js
@@ -1,3 +1,6 @@
+//!import state
+//!import utils
+
 init = function() {
     commonInitProcedures()
 }
@@ -7,11 +10,19 @@ update = function() {
 
     if (turn > 10) move('right')
 
+    if (canShield()) {
+        array1 = findEntities(IS_OWNED_BY_ME, BOT, false)
+        for (i = 0; i < size(array1); i++) {
+            fx = getX(array1[i])
+            fy = getY(array1[i])
+            if (fy < 2 || fy > arenaHeight-3) {
+                if (canShield(array1[i])) shield(array1[i])
+            }
+        }
+        shield()
+    }
     if (canReflect()) {
         reflect();
-    }
-    if (canShield()) {
-        shield();
     }
     if (y < arenaHeight/2) {
         moveTo(0, 0);
