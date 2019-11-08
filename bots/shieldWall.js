35fead9462104e840cc396e33cf333def3979133
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2019-11-08 23:41:25 +0200

    ShieldWall improved teleports, added zapping option

--- bots/shieldWall.js
+++ bots/shieldWall.js
@@ -9,34 +9,43 @@ update = function() {
     commonStateUpdates()
 
     // Start specials
-    if (turn == 1 && canReflect()) reflect()
+    //if (turn == 1 && canReflect()) reflect()
 
     // Normal actions
     if (countEnemyBotsWithMeleeCardinality(x, y) >= 1) {
-        tryTeleport(x-3, y-2)
-        tryTeleport(x-4, y-1)
-        tryTeleport(x-5, y)
+        if (isZapping()) tryToBreakMeleeCardinalityByMoving()
+        tryDefensiveTeleport()
+        if (canZap()) zap()
+        mineLureRetreat()
     }
     if (isReflecting()) {
         if (currDistToClosestBot >= 5) m(x+1, y)
         if (currDistToClosestBot == 4) idleJobs()
-        if (currDistToClosestBot <= 3) m(x-1, y)
+        if (currDistToClosestBot <= 3) {
+            if (canLayMine()) layMine()
+            m(x-1, y)
+        }
         idleJobs()
     }
     if (!isReflecting()) {
         if (!canReflect()) {
             if (currDistToClosestBot <= 5) {
-                tryTeleport(x-5, y)
-                tryTeleport(x-4, y+1)
-                tryTeleport(x-4, y-1)
-                m(x-1, y)
+                tryDefensiveTeleport()
+                mineLureRetreat()
             }
             idleJobs()
         }
         if (canReflect()) {
             if (currDistToClosestBot <= 6) reflect()
-            if (!areSensorsActivated() && canActivateSensors()) activateSensors()
-            if (areSensorsActivated()) m(x+1, y)
+
+            // Advance by laying mines, without waiting for sensors
+            if (canLayMine()) layMine()
+            m(x+1, y)
+
+            //Advance with sensors only
+            //if (!areSensorsActivated() && canActivateSensors()) activateSensors()
+            //if (areSensorsActivated()) m(x+1, y)
+
             idleJobs()
         }
     }
@@ -44,7 +53,49 @@ update = function() {
     thisShouldNeverExecute()
 }
 
+mineLureRetreat = function() {
+    if (canLayMine()) layMine()
+    tryToBreakMeleeCardinalityByMoving()
+    m(x-1, y)
+}
+
+tryToBreakMeleeCardinalityByMoving = function() {
+    moveIfNoMeleeCardinality(x-1, y)
+    moveIfNoMeleeCardinality(x, y+1)
+    moveIfNoMeleeCardinality(x, y-1)
+    moveIfNoMeleeCardinality(x+1, y)
+}
+
+tryDefensiveTeleport = function() {
+    // Prefer 5 dist jumps backward
+    tryTeleport(x-5, y)
+    tryTeleport(x-4, y-1)
+    tryTeleport(x-4, y+1)
+    tryTeleport(x-3, y-2)
+    tryTeleport(x-3, y+2)
+    tryTeleport(x-2, y-3)
+    tryTeleport(x-2, y+3)
+    tryTeleport(x-1, y-4)
+    tryTeleport(x-1, y+4)
+    // Shorter jumps backward
+    tryTeleport(x-4, y)
+    tryTeleport(x-3, y-1)
+    tryTeleport(x-3, y+1)
+    tryTeleport(x-2, y-2)
+    tryTeleport(x-2, y+2)
+    tryTeleport(x-3, y)
+    tryTeleport(x-1, y-3)
+    tryTeleport(x-1, y+3)
+    // Long jumps forward or vertically
+    tryTeleport(x+5, y)
+    tryTeleport(x+4, y+1)
+    tryTeleport(x+4, y-1)
+    tryTeleport(x, y+5)
+    tryTeleport(x, y-5)
+}
+
 idleJobs = function() {
+    if (canLayMine()) layMine()
     tryToRepairSomeoneWithoutMoving()
     wait()
 }
