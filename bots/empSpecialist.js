a5c201d1d698df87e6ee8a9a387c0dc6843d1fa1
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2019-11-16 15:01:59 +0200

    EmpSpecialist improvements

--- bots/empSpecialist.js
+++ bots/empSpecialist.js
@@ -8,14 +8,18 @@ init = function() {
 update = function() {
     commonStateUpdates()
 
+    darklingSpecial()
+
     //baitSpecial()
 
-    if (turn == 8 && canEmp()) emp('ZAPPER')
+    if (canShield()) {
+        lowestLifeFriendly = findEntity(IS_OWNED_BY_ME, BOT, SORT_BY_LIFE, SORT_ASCENDING)
+        if (exists(lowestLifeFriendly) && canShield(lowestLifeFriendly)) shield(lowestLifeFriendly)
+        shield()
+    }
     if (canReflect() && dmgTaken > 100 && currDistToClosestBot > 1) reflect()
     if (willMissilesHit()) fireMissiles()
-    if (turn >= 2) {
-        m(x+1, y)
-    }
+    m(x+1, y)
 }
 
 baitSpecial = function() {
@@ -28,4 +32,23 @@ baitSpecial = function() {
     if (turn <= 3) m(x-1, y)
     if (turn >= 8 && canEmp()) emp('MELEE')
     if (canReflect()) reflect()
+}
+
+darklingSpecial = function() {
+    if (turn == 1) {
+        if (canShield()) shield()
+        if (canReflect()) reflect()
+    }
+    if (turn == 2) {
+        tryTeleport(x-5, y)
+    }
+    if (turn <= 4) {
+        m(x-1, y)
+    }
+    if (turn <= 6) {
+        if (willMissilesHit()) fireMissiles()
+    }
+    if (turn == 7) {
+        if (canEmp()) emp('MELEE')
+    }
 }
